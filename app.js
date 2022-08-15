d235da36fdde6dd28fcc9bce9a2260a35e3d3111
Christopher Lipovsky <lipovskychris@gmail.com>
2022-08-14 20:39:36 -0400

    made some changes

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ document.getElementById("welcome").innerHTML = greeting();
 
 function greeting()
 {
-    let greetings = ["Howdy", "Hello", "Salutations", "Welcome", "Hi", "Nice to meet you", "G'day", "Hey"];
+    let greetings = ["Howdy", "Hello", "Welcome", "Hi", "G'day", "Hey"];
     return greetings[Math.floor(Math.random() * greetings.length)];
 }
